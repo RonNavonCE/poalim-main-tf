459d59af5d4c5b23048184eb585ed5774ee1fd84
RonNavonGit <ronyhezkel@gmail.com>
2023-05-16 18:14:35 +0300

    Update main.tf

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -45,51 +45,49 @@ module "dev" {
       name  = "string"
       state = "POLICY STATE - OPTIONS: enabled / disabled / enabledForReportingButNotEnforced"
       conditions = {
-        client_app_types = ["LIST OF APPS TYPES INCLUDED IN THE POLICY - OPTIONS: all / browser / mobileAppsAndDesktopClients / exchangeActiveSync / easSupported / other"]
-        sign_in_risk_levels = ["OPTIONS: low / medium / high / hidden / none / unknownFutureValue"]
-        user_risk_levels = ["OPTIONS: low / medium / high / hidden / none / unknownFutureValue"]
+        client_app_types    = ["LIST OF APPS TYPES INCLUDED IN THE POLICY - OPTIONS: all , browser , mobileAppsAndDesktopClients , exchangeActiveSync , easSupported , other"]
+        sign_in_risk_levels = ["OPTIONS: low , medium , high , hidden , none , unknownFutureValue"]
+        user_risk_levels    = ["OPTIONS: low , medium , high , hidden , none , unknownFutureValue"]
         applications = {
           excluded_applications = optional(list(string), null)
           included_applications = optional(list(string), null)
           included_user_actions = optional(list(string), null)
         }
+        devices_rule = {
+          mode = "include / exclude MATCHING DEVICIES FROM THE POLICY"
+          rule = "CONDITIONAL FILTER TO MATCH DEVICES - FOR INFO: https://learn.microsoft.com/en-us/azure/active-directory/conditional-access/concept-condition-filters-for-devices#supported-operators-and-device-properties-for-filters"
+        }
+        USERS = {
+          excluded_groups = ["!!OPTIONAL!!"]
+          excluded_roles  = ["!!OPTIONAL!!"]
+          excluded_users  = ["!!OPTIONAL!!"]
+          included_groups = ["!!OPTIONAL!!"]
+          included_roles  = ["!!OPTIONAL!!"]
+          included_users  = ["!!OPTIONAL!!"]
+        }
+        locations = {
+          excluded_locations = ["!!OPTIONAL!!"]
+          included_locations = ["LIST OF LOCATIONS IDs IN SCOPE OF THE POLICY, CAN ALSO USE - All / AllTrusted"]
+        }
+        platforms = {
+          included_platforms = ["LIST OF PLATFORMS THE POLICY APPLIES TO, OPTIONS - all, android, iOS, linux, macOS, windows, windowsPhone, unknownFutureValue"]
+          excluded_platforms = ["!!OPTIONAL!! OPTIONS - all, android, iOS, linux, macOS, windows, windowsPhone, unknownFutureValue"]
+        }
+      }
+      grant_controls = {
+        built_in_controls             = ["LIST OF BUILT-IN CONTROLS REQUIRED BY THE POLICY, OPTIONS - block, mfa, approvedApplication, compliantApplication, compliantDevice, domainJoinedDevice, passwordChange, unknownFutureValue"]
+        operator                      = "DEFINE THE RELATIONSHIP OF THE GRANT CONTROLS, OPTIONS - AND / OR"
+        custom_authentication_factors = ["!!OPTIONAL!! LIST OF CUSTOM CONTROLS IDs REQUIRED BY THE POLICY"]
+        terms_of_use                  = ["!!OPTIONAL!! LIST OF TERMS OF USE IDs REQUIRED BY THE POLICY"]
+      }
+      session_controls = {
+        application_enforced_restrictions_enabled = false # !!OPTIONAL!! ENFORCE OR NOT APP RESTRICTIONS
+        cloud_app_security_policy                 = "!!OPTIONAL!! ENABLE APP SECURITY AND SPECIFIES THE CLOUD APP SECURITY POLICY TO USE, OPTIONS - blockDownloads / mcasConfigured / monitorOnly / unknownFutureValue"
+        persistent_browser_mode                   = "!!OPTIONAL!! DEFINE WHETHER TO PERSIST COOKIES OR NOT, OPTIONS - always / never"
+        sign_in_frequency                         = 10 # !!OPTIONAL!! NUMBER OF DAYS OR HOURS TO ENFORCE SIGN-IN FREQUENCY
+        sign_in_frequency_period                  = "!!OPTIONAL!! THE TIME PERIOD TO ENFORCE SIGN-IN FREQUENCY, OPTIONS - hours / days"
       }
 
-
-        devices_rule = optional(object({
-          mode = string
-          rule = string
-        }))
-        users = object({
-          excluded_groups = optional(list(string), null)
-          excluded_roles  = optional(list(string), null)
-          excluded_users  = optional(list(string), null)
-          included_groups = optional(list(string), null)
-          included_roles  = optional(list(string), null)
-          included_users  = optional(list(string), null)
-        })
-        locations = object({
-          excluded_locations = optional(list(string), null)
-          included_locations = list(string)
-        })
-        platforms = object({
-          included_platforms = list(string)
-          excluded_platforms = optional(list(string), null)
-        })
-      })
-      grant_controls = object({
-        built_in_controls             = list(string)
-        operator                      = string
-        custom_authentication_factors = optional(list(string), null)
-        terms_of_use                  = optional(list(string), null)
-      })
-      session_controls = optional(object({
-        application_enforced_restrictions_enabled = optional(bool, false)
-        cloud_app_security_policy                 = optional(string, null)
-        persistent_browser_mode                   = optional(string, null)
-        sign_in_frequency                         = optional(number, null)
-        sign_in_frequency_period                  = optional(string, null)
-      }))
     }
   ]
 }
